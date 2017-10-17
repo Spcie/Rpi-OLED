56c342eb1f98d9c8c325b07158084aec1b323d0b
Spcie <m15188159686@163.com>
2017-10-17 20:23:46 +0800

    Create hw_delay.h

---
+++ hw_delay.h
@@ -0,0 +1,12 @@
+#ifndef __HW_DELAY_H
+#define __HW_DELAY_H
+
+#define BCM_ST_CS 			0x0000 //System Timer Control/Status
+#define BCM_ST_CLO 			0x0004 // System Timer Counter Lower 32 bits
+#define BCM_ST_CHI 			0x0008 // System Timer Counter Upper 32 bits
+
+int bcm_st_init(volatile unsigned int* peripherals_base);
+void bcm_st_delay_us(unsigned long long us);
+void bcm_st_delay_ms(unsigned int ms);
+
+#endif
