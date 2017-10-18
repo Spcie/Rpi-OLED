31f0882a088252d6c25b376080a39ce2c1f23b2d
Spcie <m15188159686@163.com>
2017-10-18 23:16:30 +0800

    update

--- oled.c
+++ oled.c
@@ -271,6 +271,6 @@ void debug_text(void)
 {
   volatile unsigned int* bcm_peripherals_base = (volatile unsigned int *)ioremap(0x3f000000, 0x01000000);
   bcm_gpio_init(bcm_peripherals_base);
-  bcm_gpio_fsel(RPI_GPIO_02,BCM_GPIO_FSEL_OUTP);
-  bcm_gpio_set(RPI_GPIO_02);
+  bcm_gpio_fsel(RPI_GPIO_03,BCM_GPIO_FSEL_OUTP);
+  bcm_gpio_set(RPI_GPIO_03);
 }
